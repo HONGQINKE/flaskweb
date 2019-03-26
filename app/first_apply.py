7f4046f514a0a152322ea0634c7596a02f989d71
HONGQINKE <815653824@qq.com>
2019-03-26 10:36:03 +0800

    点击登陆

--- app/first_apply.py
+++ app/first_apply.py
@@ -14,11 +14,13 @@ desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '7.0'
 #华硕手机
-#desired_caps['deviceName'] = 'HBAXJR00K412NC9'
+desired_caps['deviceName'] = 'HBAXJR00K412NC9'
 #小米手机、
-desired_caps['deviceName'] = 'albf48c'
+#desired_caps['deviceName'] = 'albf48c'
 desired_caps['app'] = PATH('E:\package\dev-sit.20190129121419.apk')
 desired_caps['appWaitActivity'] = 'com.kpt.android.activity.StartActivity'
+#
+#desired_caps['noReset'] = 'true'
 #desired_caps['appPackage'] = 'com.kpt.andriod'
 #desired_caps['appActivity'] = 'com.kpt.android/.activity.StartActivity'
 #desired_caps['appActivity'] = 'com.kpt.android/.activity.LeaderActivity '
@@ -69,16 +71,29 @@ swipLeft(1000)
 #点击Mulai Coba
 driver.find_element_by_id('com.kpt.android:id/startTxt').click()
 time.sleep(5)
+#小米手机
 #确认允许手机定位
-driver.find_element_by_id('android:id/button1').click()
+#driver.find_element_by_id('android:id/button1').click()
 #确认允许录制照片和视频
-driver.find_element_by_id('android:id/button1').click()
+#driver.find_element_by_id('android:id/button1').click()
 #确认获取手机号码，IMEI,IMSI权限
-driver.find_element_by_id('android:id/button1').click()
+#driver.find_element_by_id('android:id/button1').click()
+
+time.sleep(5)
+#华硕手机
+#确认允许手机定位
+driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+#确认允许录制照片和视频
+driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+#确认获取手机号码，IMEI,IMSI权限
+driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
+
+logging.info(driver.page_source)
 #输入手机号
 driver.find_element_by_id('com.kpt.android:id/phoneEdit').send_keys('9999990001')
 #点击登录
 driver.find_element_by_id('com.kpt.android:id/loginBtn').click()
+
 #连接数据库获取数据库中验证码
 mysql = Mysql()
 sql = "select CODE from pub_verification_code where phone =9999990001 order by CREATE_TIME desc LIMIT 0,1"
@@ -92,13 +107,16 @@ code_list = list(code)
 print(code_list)
 
 #输入验证码
+time.sleep(10)
+
+driver.find_element_by_id('com.kpt.android:id/code1Edit').click()
+driver.press_keycode(int(code_list[0])+7)
+driver.press_keycode(int(code_list[1])+7)
+driver.press_keycode(int(code_list[2])+7)
+driver.press_keycode(int(code_list[3])+7)
 time.sleep(5)
-#xf = driver.find_elements_by_class_name('android.widget.EditText')
-driver.switch_to.frame(0)
-driver.find_element_by_id('com.kpt.android:id/code1Edit').send_keys(code_list[0])
-driver.find_element_by_id('com.kpt.android:id/code2Edit').send_keys(code_list[1])
-driver.find_element_by_id('com.kpt.android:id/code3Edit').send_keys(code_list[2])
-driver.find_element_by_id('com.kpt.android:id/code4Edit').send_keys(code_list[3])
+#点击登录
+driver.find_element_by_id('com.kpt.android:id/loginBtn').click()
 
 #退出
 driver.quit()
